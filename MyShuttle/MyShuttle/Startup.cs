b94b378b1161544f600f474031120a96de8f010b
Delfim Baltazar <deltavivo@gmail.com>
2018-08-22 17:04:49 -0300

    .net core controllers

--- MyShuttle/MyShuttle/Startup.cs
+++ MyShuttle/MyShuttle/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using MyShuttle.Web.AppBuilderExtensions;
 
 namespace MyShuttle
 {
@@ -15,6 +16,8 @@ namespace MyShuttle
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.ConfigureDependencies();
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,10 +28,20 @@ namespace MyShuttle
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
+            /* app.Run(async (context) =>
+             {
+                 await context.Response.WriteAsync("Hello World!");
+             });*/
+
+            app.UseMvc(routes =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                routes.MapRoute(
+                    name: "Default",
+                    template: "{controller}/{action}/{id?}",
+                    defaults: new { controller = "Home", action = "Index" }
+                    );
             });
+
         }
     }
 }
